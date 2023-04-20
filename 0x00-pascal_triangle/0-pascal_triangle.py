e783ef4f18f22e14af0e728083136f71320f4413
Cemordi2 <emordichukwuma@gmail.com>
2023-04-20 11:07:42 +0100

    A script that determine pascal traingle for any number

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/user/bin/python3
+
+""" A script that determine pascal traingle for any number"""
+
+
+def pascal_triangle(n):
+
+    """ A function that returns a list of lists of numbers"""
+    if n <= 0:
+        return []
+    pascal_tria = []
+    for i in range(n):
+        for j in range(n):
+            temp_ar = []
+            if i == 0:
+                temp_ar.append(1)
+            if i == 1:
+                temp_ar.append(1)
+            else:
+                temp_ar.append([pascal_tria[i-1][j-1] + pascal_tria[i-1][j]])
+        pascal_tria.append(temp_ar)
+    return pascal_tria
