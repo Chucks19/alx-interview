4ea1fecfd36b9b39c2c598291c3598c476e5bd94
Cemordi2 <emordichukwuma@gmail.com>
2023-06-10 20:53:30 +0100

    update

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,55 +1,59 @@
 #!/usr/bin/python3
-"""Summary of Matrix rotation
+"""_summary_
 """
 
 
-def transpose_matx(matrix, n):
+def transpose_matrix(matrix, n):
     """_summary_
 
     Args:
-       description_
+                    matrix (_type_): _description_
     """
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
 
-def reverse_matx(matrix):
+def reverse_matrix(matrix):
     """_summary_
 
     Args:
-               description_
+                    matrix (_type_): _description_
     """
     for row in matrix:
         row.reverse()
 
 
-def rotate_2d_matx(matrix):
+def rotate_2d_matrix(matrix):
     """_summary_
 
     Args:
-               _description_
+                    matrix (_type_): _description_
     """
     n = len(matrix)
     # print(n)
 
     """sample matrix
     1 2 3
+    4 5 6
+    7 8 9
     """
 
-    # traSpose matrix
+    # transpose matrix
     """
     1 4 7
+    2 5 8
     3 6 9
     """
 
-    transpose_matx(matrix, n)
+    transpose_matrix(matrix, n)
 
     # reverse matrix
     """
     7 4 1
+    8 5 2
+    9 6 3
     """
-    reverse_matx(matrix)
+    reverse_matrix(matrix)
 
     return matrix
-
